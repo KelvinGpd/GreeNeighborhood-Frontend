4f8fb718b1e454e14c5840ac687391bd4ca1bb39
Kelvin Chen <kkcsike@gmail.com>
2024-01-20 20:48:19 -0500

    initial layout of home page

--- src/websitePages/HomePageComponents/HomePageBottom.jsx
+++ src/websitePages/HomePageComponents/HomePageBottom.jsx
@@ -1,4 +1,18 @@
+import Constants from "../../data/constants";
+import Button from "../../globalComponents/Button";
+import MapPage from "../MapPage";
+
 const HomePageBottom = ({setActivePage}) => {
+    return (
+        <div className="home-page-bottom">
+            <div>{Constants.SOURCE}</div>
+            <div>{Constants.SOURCE_DESCRIPTION}</div>
+            <Button
+                text = {Constants.HOMEPAGE_BUTTON}
+                onClick = {() => setActivePage(<MapPage/>)}
+            />
+        </div>
+    )
 
 }
 export default HomePageBottom;
